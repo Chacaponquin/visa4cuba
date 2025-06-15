dcf940ab60cf51db9483a38a0d0d3540e572c4e5
Héctor Gómez <hectorangel2001@gmail.com>
2025-06-15 00:15:54 -0400

    feat: add background image

--- src/pages/AboutUs/components/Header/Header.tsx
+++ src/pages/AboutUs/components/Header/Header.tsx
@@ -1,13 +1,19 @@
 export default function Header() {
   return (
-    <header className="flex w-full flex-col items-center py-32 bg-primary-100 text-center px-5">
+    <header
+      style={{
+        background:
+          "linear-gradient(to top, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url('/images/cuba-2.jpg') center/cover",
+      }}
+      className="flex w-full flex-col items-center justify-center py-44 text-center px-5"
+    >
       <div className="flex flex-col items-center w-full max-w-3xl">
         <div className="text-6xl mb-8">
-          <h1 className="font-title-bold mb-1">Descubre Cuba con</h1>
+          <h1 className="font-title-bold mb-1 text-white">Descubre Cuba con</h1>
           <h1 className="font-title-bold text-secondary">Visa4Cuba</h1>
         </div>
 
-        <p className="text-lg text-gray-600">
+        <p className="text-lg text-gray-200">
           Somos una agencia de viajes especializada en crear experiencias únicas
           e inolvidables en la hermosa isla de Cuba. Con más de 10 años de
           experiencia, te ayudamos a descubrir la auténtica cultura, historia y
