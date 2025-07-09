4cfd0185903661e10efd9eeed3c67717272a5301
Héctor Gómez <hectorangel2001@gmail.com>
2025-07-08 20:38:34 -0400

    feat: optimize images

--- src/modules/app/domain/constants/app-images.ts
+++ src/modules/app/domain/constants/app-images.ts
@@ -1,11 +1,11 @@
 export const APP_IMAGES = {
   LOGO: { src: "/images/logo.png", alt: "Logo" },
   FOOTER_LOGO: { src: "/images/logo-footer.svg", alt: "Footer logo" },
-  CUBA_1: { src: "/images/cuba-1.jpg", alt: "cuba-1" },
-  CUBA_2: { src: "/images/cuba-2.jpg", alt: "cuba-2" },
-  CUBA_3: { src: "/images/cuba-3.jpg", alt: "cuba-3" },
-  CUBA_4: { src: "/images/cuba-4.jpg", alt: "cuba-4" },
-  HEALTH: { src: "/images/health.jpg", alt: "health" },
-  BEACH: { src: "/images/beach.jpg", alt: "beach" },
-  DOCTOR: { src: "/images/doctor.jpg", alt: "doctor" },
+  CUBA_1: { src: "/images/cuba-1.webp", alt: "cuba-1" },
+  CUBA_2: { src: "/images/cuba-2.webp", alt: "cuba-2" },
+  CUBA_3: { src: "/images/cuba-3.webp", alt: "cuba-3" },
+  CUBA_4: { src: "/images/cuba-4.webp", alt: "cuba-4" },
+  HEALTH: { src: "/images/health.webp", alt: "health" },
+  BEACH: { src: "/images/beach.webp", alt: "beach" },
+  DOCTOR: { src: "/images/doctor.webp", alt: "doctor" },
 };
