bd3dd93cbbb6071b58422303bfd1e213f74be82a
Héctor Gómez <hectorangel2001@gmail.com>
2025-07-10 22:55:08 -0400

    feat: init translate home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,7 @@
+import BaseTranslationPage from "../../modules/app/components/BaseTranslationPage/BaseTranslationPage";
 import { APP_IMAGES } from "../../modules/app/domain/constants/app-images";
 import type { TranslationPageProps } from "../../modules/app/domain/core/translation-route";
+import useTranslation from "../../modules/app/modules/language/hooks/useTranslation";
 import LayoutHeader from "../../modules/shared/components/Layout/components/LayoutHeader/LayoutHeader";
 import Layout from "../../modules/shared/components/Layout/Layout";
 import Services from "../../modules/shared/components/Services/Services";
@@ -8,20 +10,35 @@ import Information from "./components/Information/Information";
 import Steps from "./components/Steps/Steps";
 import Why from "./components/Why/Why";
 
-export default function Home({ builder }: TranslationPageProps) {
-  return (
-    <Layout builder={builder}>
-      <LayoutHeader
-        image={APP_IMAGES.BEACH.src}
-        description="Obtén tu visa turística y seguro de viaje para Cuba en minutos. Proceso 100% digital, soporte 24/7 y garantía de aprobación"
-        title="Visas y Seguros para Cuba de forma rápida con"
-      />
+export default function Home({ builder, language }: TranslationPageProps) {
+  const { TITLE, DESCRIPTION } = useTranslation({
+    TITLE: {
+      es: "Visas y Seguros para Cuba de forma rápida con",
+      en: "Visas and Insurance for Cuba quickly with",
+      it: "Visti e assicurazione per Cuba rapidamente con",
+    },
+    DESCRIPTION: {
+      es: "Obtén tu visa turística y seguro de viaje para Cuba en minutos. Proceso 100% digital, soporte 24/7 y garantía de aprobación",
+      en: "Get your tourist visa and travel insurance for Cuba in minutes. 100% digital process, 24/7 support, and approval guaranteed.",
+      it: "Ottieni il visto turistico e l'assicurazione di viaggio per Cuba in pochi minuti. Processo digitale al 100%, assistenza 24 ore su 24, 7 giorni su 7 e approvazione garantita.",
+    },
+  });
 
-      <Services />
-      <Steps />
-      <Information />
-      <Faq />
-      <Why />
-    </Layout>
+  return (
+    <BaseTranslationPage language={language} builder={builder}>
+      <Layout builder={builder}>
+        <LayoutHeader
+          image={APP_IMAGES.BEACH.src}
+          description={DESCRIPTION}
+          title={TITLE}
+        />
+
+        <Services />
+        <Steps />
+        <Information />
+        <Faq />
+        <Why />
+      </Layout>
+    </BaseTranslationPage>
   );
 }
--- src/pages/Home/components/Faq/Faq.tsx
+++ src/pages/Home/components/Faq/Faq.tsx
@@ -4,6 +4,7 @@ import type { TabOption } from "../../../../modules/app/modules/ui/components/Ta
 import Tabs from "../../../../modules/app/modules/ui/components/Tabs/Tabs";
 import VisaFaq from "../../../../modules/shared/components/VisaFaq/VisaFaq";
 import InsurenceFaq from "../../../../modules/shared/components/InsurenceFaq/InsurenceFaq";
+import useTranslation from "../../../../modules/app/modules/language/hooks/useTranslation";
 
 enum FAQ_SECTION {
   VISA,
@@ -13,25 +14,40 @@ enum FAQ_SECTION {
 export default function Faq() {
   const [section, setSection] = useState(FAQ_SECTION.VISA);
 
+  const { TITLE, DESCRIPTION, TAB_1, TAB_2 } = useTranslation({
+    TITLE: {
+      es: "Preguntas frecuentes",
+      en: "Frequently asked questions",
+      it: "Domande frequenti",
+    },
+    DESCRIPTION: {
+      es: "Resolvemos las dudas más comunes sobre el proceso de obtención de visas para Cuba",
+      en: "We answer the most common questions about the visa process for Cuba.",
+      it: "Rispondiamo alle domande più frequenti sulla procedura di visto per Cuba.",
+    },
+    TAB_1: { es: "Visas de viaje", en: "Travel visas", it: "Visti di viaggio" },
+    TAB_2: {
+      es: "Seguros médicos",
+      en: "Health insurance",
+      it: "Assicurazione sanitaria",
+    },
+  });
+
   const tabs: TabOption[] = [
     {
-      name: "Visas de viaje",
+      name: TAB_1,
       onClick: () => setSection(FAQ_SECTION.VISA),
       selected: section === FAQ_SECTION.VISA,
     },
     {
-      name: "Seguros médicos",
+      name: TAB_2,
       onClick: () => setSection(FAQ_SECTION.INSURANCE),
       selected: section === FAQ_SECTION.INSURANCE,
     },
   ];
 
   return (
-    <LayoutSection
-      id="faq"
-      title="Preguntas frecuentes"
-      description="Resolvemos las dudas más comunes sobre el proceso de obtención de visas para Cuba"
-    >
+    <LayoutSection id="faq" title={TITLE} description={DESCRIPTION}>
       <Tabs options={tabs} full className="mb-5" />
 
       {section === FAQ_SECTION.VISA && <VisaFaq />}
