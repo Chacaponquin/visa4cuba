6568f83642d4a3a7b0489ed7d91b3b7ffd562e3e
H G <hectorangel2001@gmail.com>
2025-08-14 16:52:20 -0400

    fix: refactor opinion section

--- src/pages/Home/components/Opinions/Opinions.tsx
+++ src/pages/Home/components/Opinions/Opinions.tsx
@@ -1,14 +1,9 @@
-import { useContext, useState } from "react";
 import useTranslation from "../../../../modules/app/modules/language/hooks/useTranslation";
 import LayoutSection from "../../../../modules/shared/components/Layout/components/LayoutSection/LayoutSection";
 import { OPINION_OPTIONS } from "./domain/opinion";
 import Card from "./components/Card/Card";
-import P from "../../../../modules/app/modules/ui/components/Markdown/components/P/P";
-import { LanguageContext } from "../../../../modules/app/modules/language/context/language-context";
 
 export default function Opinions() {
-  const { language } = useContext(LanguageContext);
-
   const { TITLE, HEADER } = useTranslation({
     TITLE: {
       en: "Let’s Explore Why People Say About Our Services",
@@ -18,28 +13,13 @@ export default function Opinions() {
     HEADER: { es: "Opiniones", en: "Opinions", it: "Opinioni" },
   });
 
-  const [selected, setSelected] = useState(OPINION_OPTIONS[1]);
-
   return (
     <LayoutSection title={TITLE} header={HEADER}>
-      <div className="flex items-center gap-x-10 mb-10">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-x-6 gap-y-20 mt-10">
         {OPINION_OPTIONS.map((o, index) => (
-          <Card
-            key={index}
-            onClick={() => setSelected(o)}
-            props={o}
-            selected={selected === o}
-          />
+          <Card key={index} props={o} />
         ))}
       </div>
-
-      <div className="text-center mb-5 w-full max-w-[800px]">
-        <P>{selected.description[language]}</P>
-      </div>
-
-      <h4 className="text-gray-800 text-xl font-title-semibold">
-        {selected.name}
-      </h4>
     </LayoutSection>
   );
 }
--- src/pages/Home/components/Opinions/components/Card/Card.tsx
+++ src/pages/Home/components/Opinions/components/Card/Card.tsx
@@ -1,26 +1,36 @@
 import clsx from "clsx";
 import type { Opinion } from "../../domain/opinion";
+import { useContext } from "react";
+import { LanguageContext } from "../../../../../../modules/app/modules/language/context/language-context";
+import P from "../../../../../../modules/app/modules/ui/components/Markdown/components/P/P";
 
 interface Props {
   props: Opinion;
-  onClick(): void;
-  selected: boolean;
 }
 
-export default function Card({ props, onClick, selected }: Props) {
+export default function Card({ props }: Props) {
+  const { language } = useContext(LanguageContext);
+
   return (
     <article
-      onClick={onClick}
-      className={clsx("transition-all duration-200 cursor-pointer", {
-        "scale-150": selected,
-        "hover:scale-110": !selected,
-      })}
+      className={clsx(
+        "transition-all duration-200 cursor-pointer shadow-md flex flex-col items-center rounded-card relative",
+        {}
+      )}
     >
       <img
         src={props.image.src}
         alt={props.name}
-        className="object-cover rounded-full sm:w-[70px] sm:h-[70px] w-[60px] h-[60px]"
+        className="object-cover rounded-full w-[80px] h-[80px] absolute top-0 -translate-y-[50%]"
       />
+
+      <div className="flex flex-col w-full px-5 py-4 items-center justify-between h-full mt-10">
+        <P>{props.description[language]}</P>
+
+        <h4 className="text-gray-800 text-lg font-title-semibold mt-4">
+          {props.name}
+        </h4>
+      </div>
     </article>
   );
 }
